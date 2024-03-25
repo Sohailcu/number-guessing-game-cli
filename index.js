da75e1f206efbfc87a8a88aa21f6fb5f54782094
Sohailcu <sohailcu@gmail.com>
2024-03-25 16:53:28 +0500

    Created Number Guessing Game

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+// 1) Computer will generate a random number    (Done)
+// 2) user input for gussing number             (Done)
+// 3) compare user input with computer generated number and show result
+// const randomnumber = Math.random();
+const randomnumber = Math.floor(Math.random() * 10 + 1); // Generate a random whole number between 1 - 10 inclusiveconst minNumber=1;
+console.log(randomnumber);
+// const randomnumber = 13; 
+const answers = await inquirer.prompt([
+    {
+        name: "Userguessednumber",
+        type: "number",
+        message: "please guess a number between 1-10 : ",
+    }
+]);
+if (answers.Userguessednumber === randomnumber) {
+    console.log("Congtatulational.! You Guessed Right Number.");
+}
+else {
+    console.log("You Guessed Wrong Number.");
+}
