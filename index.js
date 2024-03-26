68fe3d730cae30775db0cc7478dbf86602d2403f
Sohailcu <sohailcu@gmail.com>
2024-03-26 17:47:33 +0500

    Changes in Name

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 // 1) Computer will generate a random number    (Done)
 // 2) user input for gussing number             (Done)
 // 3) compare user input with computer generated number and show result
 // const randomnumber = Math.random();
 const randomnumber = Math.floor(Math.random() * 10 + 1); // Generate a random whole number between 1 - 10 inclusiveconst minNumber=1;
-console.log(randomnumber);
+console.log("Welcome to My First  Project Number Guessing Game");
+// console.log(randomnumber);
 // const randomnumber = 13; 
 const answers = await inquirer.prompt([
     {
